564109f5d150c8b4127b8e59eb19401572a84f90
Yakovenko Evgenii <link07@mail.ru>
2024-12-21 20:08:58 +0300

    v.1

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+import pandas as pd
+
+st.title("Анализ данных с использованием Streamlit")
+
+st.header("Шаг 1: Загрузка данных")
+
+uploaded_file = st.file_uploader("Выберите CSV-файл", type=["csv"])
+
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write("Превью данных:")
+    st.dataframe(data)
+else:
+    st.write("Пожалуйста, загрузите CSV-файл.")
